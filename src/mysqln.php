c7f7e0908e53aae11958ae7d80d2885d9dfd526c
Untlsn <Untellson@gmail.com>
2020-10-07 15:07:25 +0200

    Add database and query resolwer class

---
+++ src/mysqln.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class MySqlN {
+
+    private mysqli $connection;
+
+    function __construct(string $server, string $user, string $password) {
+        $this->connection = new mysqli($server, $user, $password);
+    }
+
+    function query(string $sqlCode) {
+        if(!$query = $this->connection->query($sqlCode)) {
+            throw new Exception('>ERROR Query is invalid. ERROR<');
+        }else{
+            $arr = [];
+            while($row = $query->fetch_row()) {
+                $arr[] = $row;
+            }
+            $query->close();
+            return $arr;
+        }
+    }
+
+    function __destruct(){
+        $this->connection->close();
+    }
+
+
+}
